e959cdcac508899afb8865e7f0b231a2046ec561
Mark Cristobal <markjoshcristobal24@gmail.com>
2024-03-26 12:25:41 +0800

    added property binding

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrl: './shopping-list.component.css'
+})
+export class ShoppingListComponent {
+  ingredients: Ingredient[] = [
+    new Ingredient('apple', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}
